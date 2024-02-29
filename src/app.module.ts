d3d65e787610cd9a7344a491b5f80d0ec50cde0d
babouslt <bastienselot@gmail.com>
2024-02-29 15:16:00 +0100

    feat(wishlist): added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { WishlistModule } from './wishlist/wishlist.module';
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, WishlistModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/wishlist/wishlist.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Request,
+  Body,
+  Get,
+} from '@nestjs/common';
+import { WishlistService } from './wishlist.service';
+import { AuthGuard } from '../auth/auth.guard';
+import { User } from '@prisma/client';
+
+@Controller('wishlist')
+export class WishlistController {
+  constructor(private wishlistService: WishlistService) {}
+
+  @UseGuards(AuthGuard)
+  @Post('/addItem')
+  addItem(
+    @Request() req,
+    @Body('id') productId: number,
+  ): Promise<{ message: string; data: User['wishlist'] | null }> {
+    const userId = req['user']?.id; // Récupérer l'ID du token depuis la requête
+
+    return this.wishlistService.addItemToWishlist(productId, userId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/getWishlist')
+  getWishlist(@Request() req): Promise<User['wishlist']> {
+    const userId = req['user']?.id; // Récupérer l'ID du token depuis la requête
+
+    return this.wishlistService.getWishlistProducts(userId);
+  }
+}
