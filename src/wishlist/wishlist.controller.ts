e69d3f698bc1c6dcf0eca7b1e7029daba3c6553b
babouslt <bastienselot@gmail.com>
2024-02-29 16:29:12 +0100

    feat(add): remove item from wishlist

--- src/wishlist/wishlist.controller.ts
+++ src/wishlist/wishlist.controller.ts
@@ -5,6 +5,7 @@ import {
   Request,
   Body,
   Get,
+  Delete,
 } from '@nestjs/common';
 import { WishlistService } from './wishlist.service';
 import { AuthGuard } from '../auth/auth.guard';
@@ -31,5 +32,16 @@ export class WishlistController {
     const userId = req['user']?.id; // Récupérer l'ID du token depuis la requête
 
     return this.wishlistService.getWishlistProducts(userId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete('/removeItem')
+  removeItem(
+    @Request() req,
+    @Body('id') productId: number,
+  ): Promise<{ message: string; data: User['wishlist'] | null }> {
+    const userId = req['user']?.id; // Récupérer l'ID du token depuis la requête
+
+    return this.wishlistService.removeItemFromWishlist(productId, userId);
   }
 }
