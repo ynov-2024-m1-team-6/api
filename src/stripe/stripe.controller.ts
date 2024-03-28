a9e992214fe78397f96dcf5e5b4cad75b716bc9c
Rayan Selmi <72416925+rs-4@users.noreply.github.com>
2024-03-28 14:33:11 +0100

    feat stripe

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -6,6 +6,8 @@ import {
   Body,
   Get,
   Delete,
+  RawBodyRequest,
+  Response,
 } from '@nestjs/common';
 import { StripeService } from './stripe.service';
 
@@ -21,5 +23,18 @@ export class StripeController {
   ) {
     const userId = req['user']?.id;
     return this.stripeService.createSession(data.email, data.commandId);
+  }
+
+  @Post('/refund')
+  async refundPayment(@Body() data: { paymentId: string }) {
+    return this.stripeService.refundPayment(data.paymentId);
+  }
+
+  @Post('/webhook')
+  handleWebhook(@Request() req: RawBodyRequest<Request>) {
+    const sig = req.headers['stripe-signature'] as string;
+    const endpointSecret =
+      'whsec_a89f99202fc9db06aa43695f17299b40621b79fb6640822019b7e638a31cb97b';
+    this.stripeService.handleWebhook(req.rawBody, sig, endpointSecret);
   }
 }
